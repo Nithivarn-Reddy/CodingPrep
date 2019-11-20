a511663812fc44004e620ed8e72fb864c8aeb989
sai-sreenath <sai-sreenath@users.noreply.github.com>
2019-11-20 01:38:15 -0500

    O(n) complexity sol for longestsubseqlist count

--- Sreenathsnippets/LenLongSubsequentlist.cs
+++ Sreenathsnippets/LenLongSubsequentlist.cs
@@ -1,4 +1,6 @@
- class Program
+Time complexity: O(n^2) solution
+--------------------------------
+class Program
  {
         static void Main(string[] args)
         {
@@ -35,4 +37,71 @@
         }
  }
  
- Time complexity: O(n^2) solution
+ Algorithm:
+----------
+ 1)Using Arraylist,for every character ith element in the string,traverse the all character elements from elements from ith position to end.
+  
+
+
+
+
+Time complexity: O(n) solution
+---------------------------------
+ class Longestsub
+    {
+        private static void Main(string[] args)
+        {
+            String str = "jbpnbwwd";
+            Console.WriteLine("The input string is :" + str);
+            Console.WriteLine(Longestsubseq(str));
+            Console.ReadLine();
+        }
+
+        private static int Longestsubseq(string str)
+        {
+            int n = str.Length;
+            HashSet<char> hs = new HashSet<char>();
+            int maxlen = 0, i , j ;
+            for (i = 0, j = 0; i < n && j < n;)
+            {
+                if (!hs.Contains(str[j]))
+                {
+                    hs.Add(str[j]);
+                    j = j + 1;
+                    maxlen = Math.Max(maxlen, j - i);
+                }
+                else
+                {
+                    hs.Remove(str[i]);
+                    i=i+1;
+                }
+            }
+            return maxlen;
+        }
+    }
+
+ Algorithm:
+------------
+ Hashset: Hashset is an unordered collection of unique elements. Generally used in a situation where we want to prevent duplicates
+ from being inserted in the collection. 
+ 
+  1)store each element in the hashset if traversing element is not already present.
+  2)Remove the slow pointer element if fast pointer(current) element is already present in the hashet.
+  3)At any point of time,length of subsequent string length is difference between fast pointer element and slow pointer element indices. 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
