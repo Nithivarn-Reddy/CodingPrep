c0837b7e7b23682e9c9e4c592dc613dc8a27417f
saitejareddych <chsaiteja4@gmail.com>
2019-11-21 09:52:56 -0500

    Update Nov20_FindSubstring.java

--- #include/Nov20_FindSubstring.java
+++ #include/Nov20_FindSubstring.java
@@ -1,1 +1,20 @@
-
+public class FindSubstring {
+    private static int findPattern(String s, String target) {
+        int count=0;
+        String temp="";
+        int len=target.length();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==target.charAt(0)){
+                temp = s.substring(i,i+len);
+                if(temp.compareTo( target)==0) {
+                    count++;
+                    System.out.printf("Start Index %d \n", i);
+                    i=i+len-1;
+                } } }
+        return count; }
+    public static void main(String args[]){
+        String target= "GHJ";
+        String s="GHJASDGHEFJTGHJ";
+        int n=findPattern(s,target);
+        System.out.printf("total count %d \n",n);
+    }}
