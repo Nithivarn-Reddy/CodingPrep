92b281fca6b07a6c403bbfb891f0a24342a99771
saitejareddych <chsaiteja4@gmail.com>
2019-11-23 19:42:00 -0500

    Update Nov22_RemoveAdjDuplicates.java

--- #include/Nov22_RemoveAdjDuplicates.java
+++ #include/Nov22_RemoveAdjDuplicates.java
@@ -1,1 +1,18 @@
-
+class RemoveAdjDuplicate {
+    static String removeD(String s){
+        int i=0;
+        for(int j=0;j<s.length();j++){
+            if(s.charAt(i)!=s.charAt(j))
+                i++;
+            if(s.charAt(i)!=s.charAt(j) && i!=j){
+                String a=s.substring(i-1,j);
+                s=s.replace(a,"");
+                i=0;j=0;
+            }}
+        return s;
+    }
+    public static void main(String args[]){
+        String s="abbabaaca";
+        String r=removeD(s);
+        System.out.println(r);
+    }}
